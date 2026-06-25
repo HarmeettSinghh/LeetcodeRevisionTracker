71870aa25c5245e01a893b00d730234a978d3f74
Harmeet Singh <hs260457@gmail.com>
2026-06-25 14:17:28 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.14%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,29 +1,39 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
-        int[] arr = new int[n];
+        int[]  arr = new int[n];
+        int start =0, end = 0;
+        int sum =0;
         if(k==0){
             for(int i =0; i<n; i++){
-                arr[i] =0;
-            }
+            arr[i] =0;
+    
+           }
+           return arr;
         }
         else if(k>0){
-            for(int i=0; i<n; i++){
-                int sum =0;
-                for(int j=1; j<=k; j++){
-                    sum+=code[(i+j)%n];
-                }
-                arr[i] = sum;
-            }
+            start = 1;
+            end =k;
+
         }
         else{
-            for(int i =0; i<n; i++){
-                int sum =0;
-                for(int j=1; j<=-k; j++){
-                    sum+=code[(i-j+n)%n];
-                }
-                arr[i] = sum;
-            }
+            k=-k;
+            start = n-k;
+            end = n-1;
+            
+
+        }
+        //calculating the sum of the first window
+        for(int i = start; i<=end; i++){
+            sum+=code[i%n];
+        }
+        for(int i =0; i<n; i++){
+            arr[i] = sum;
+            sum-=code[start%n];
+            start++;
+            end++;
+            sum+=code[end%n];
+
         }
         return arr;
     }
